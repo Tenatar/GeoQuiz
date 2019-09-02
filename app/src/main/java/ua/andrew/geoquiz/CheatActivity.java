d59bc5ec9c7c47b6f079ed8a6a6bda6019d2826d
Andrew <andrewart.kaplii@gmail.com>
2019-09-02 14:38:34 +0300

    added animation on CheatActivity, and displaying API level

--- app/src/main/java/ua/andrew/geoquiz/CheatActivity.java
+++ app/src/main/java/ua/andrew/geoquiz/CheatActivity.java
@@ -1,11 +1,15 @@
 package ua.andrew.geoquiz;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewAnimationUtils;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -15,6 +19,7 @@ public class CheatActivity extends AppCompatActivity {
     private boolean mAnswerIsTrue;
     private TextView mAnswerTextView;
     private Button mShowAnswer;
+    private TextView mApiLevelTextView;
     public static Intent  newIntent(Context packageContext, boolean answerIsTrue){
         Intent i=new Intent(packageContext, CheatActivity.class);
         i.putExtra(EXTRA_ANSWER_IS_TRUE, answerIsTrue);
@@ -32,6 +37,8 @@ public class CheatActivity extends AppCompatActivity {
         mAnswerIsTrue=getIntent().getBooleanExtra(EXTRA_ANSWER_IS_TRUE, false);
         mAnswerTextView=(TextView) findViewById(R.id.answer_text_view);
         mShowAnswer=(Button) findViewById(R.id.show_answer_button);
+        mApiLevelTextView=(TextView) findViewById(R.id.api_level_text_view);
+        mApiLevelTextView.setText(Build.VERSION.SDK_INT);
         mShowAnswer.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -41,6 +48,24 @@ public class CheatActivity extends AppCompatActivity {
                     mAnswerTextView.setText(R.string.false_button);
                 }
                 setAnswerShownResult(true);
+                if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    int cx = mShowAnswer.getWidth() / 2;
+                    int cy = mShowAnswer.getHeight() / 2;
+                    float radius = mShowAnswer.getWidth();
+                    Animator anim = ViewAnimationUtils.createCircularReveal(mShowAnswer, cx, cy, radius, 0);
+                    anim.addListener(new AnimatorListenerAdapter() {
+                        @Override
+                        public void onAnimationEnd(Animator animation) {
+                            super.onAnimationEnd(animation);
+                            mAnswerTextView.setVisibility(View.VISIBLE);
+                            mShowAnswer.setVisibility(View.INVISIBLE);
+                        }
+                    });
+                    anim.start();
+                }else{
+                    mAnswerTextView.setVisibility(View.VISIBLE);
+                    mShowAnswer.setVisibility(View.INVISIBLE);
+                }
 
             }
         });
